bab281e977e72c0de2ed18f27be05eaec8f82c36
Mir Tanvir Ahmed <mrmirofficial@gmail.com>
2021-06-14 20:45:01 +0600

    Default adminpanel

---
+++ routes/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Adminpanel\PageController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')
+    ->middleware(['auth', 'admin'])
+    ->as('admin.')
+    ->group(function () {
+        Route::get('/', [PageController::class, 'index'])
+  
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Userpanel\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', [PageController::class, 'home'])
-    ->name('userpanel.home');
-Route::redirect('/', '/home');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
+require __DIR__ . '/admin.php';
