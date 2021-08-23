05a90614407fcc13243ec3cce6f5afc30fbdf48f
Mir Tanvir Ahmed <mrmirofficial@gmail.com>
2021-08-23 16:52:19 +0600

    fixed all categories design

--- resources/views/userpanel/questionCategories/index.blade.php
+++ resources/views/userpanel/questionCategories/index.blade.php
@@ -2,43 +2,48 @@
 
 @section('content')
 <div class="container my-5 ">
-    <div class="card shadow">
-        <div class="card-header">
-            <div class="heading-2 p-0">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSchRC18LkNEmqtEoH-Pj-2yT-vC1hx06fnEQ&usqp=CAU"
-                    alt="" class="" height="40">
-                All categories
-            </div>
-        </div>
-        <div class="card-body">
-            <div class="row justify-row">
+    
+
+<div style="display: flex; 
+    justify-content: space-between;
+    align-items: center;    
+    flex-wrap: wrap;">
                 @for ($i = ord('a'); $i <= ord('z'); $i++) @if($questionCategories->filter(
                     fn ($questionCategory, $key) => str_starts_with(strtolower($questionCategory->name),chr($i))
                     )->count() > 0)
 
-                    <div class="col-md-4">
-                        <div class="heading">
+                    <div class="d-flex" style="margin: 20px 0; width: 300px; height: {{ 
+
+$questionCategories->filter(
+                            fn ($questionCategory, $key) => str_starts_with(strtolower($questionCategory->name),
+                            chr($i))
+                            )->count() * 50
+
+                     }}px">
+                        <div class="heading p-0 px-3 text-white __flex-center" style="background:#969494">
                             {{ strtoupper(chr($i)) }}
                         </div>
-                        <ul class="list-group mt-3">
+                        <ul class="list-group w-100">
                             @foreach ($questionCategories->filter(
                             fn ($questionCategory, $key) => str_starts_with(strtolower($questionCategory->name),
                             chr($i))
                             ) as $questionCategory)
-                            <li class="list-group-item d-flex justify-content-between align-items-start" style="background:#f2f0f0">
+                            <li class="list-group-item d-flex " style="background:#f2f0f0;justify-content: space-between;">
                                 @if ($questionCategory->staticPageExists)
-                                <a href="{{ '/question-category/'.$questionCategory->slug.'.html' }}" style="color:#969494">
+                                <a href="{{ '/question-category/'.$questionCategory->slug.'.html' }}" style="color:#615b5b">
                                     {{ $questionCategory->name }}
                                 </a>
                                 @else
                                 <a href="{{ route('userpanel.questionCategory.all-questions', $questionCategory->slug) }}"
-                                style="color:#969494">
+                                style="color:#615b5b">
                                     {{ $questionCategory->name }}
                                 </a>
                                 @endif
+                                <div class="">
                                 <span class="badge rounded-pill" style="background:#969494">
                                     {{ $questionCategory->questions()->count() }}
                                 </span>
+                                </div>
                             </li>
                             @endforeach
                         </ul>
@@ -47,6 +52,4 @@
                     @endif
                     @endfor
             </div>
-        </div>
-    </div>
 </div> @endsection
